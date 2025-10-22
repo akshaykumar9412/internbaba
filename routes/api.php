3c59e9bd9b15ab4ffffab96478d7a15ab09b8a6a
akshaykumar9412 <1234akshaykumar9412@gmail.com>
2025-10-22 12:38:35 +0530

    Introduction

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/test', function (Request $request) {
-    return response()->json(['message' => 'API is working',"data"=>$request->all()]);
+    return response()->json(['message' => 'API is working', "data" => $request->all()]);
 });
--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+//Hi my name is Akshay Kumar. I am a web developer and a student of computer science engineering. I love to code and develop new things.
